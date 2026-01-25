aabc0cc9bf6155e8f6dd7b6d157b0feb809d7fdc
oe3jtb <oe3jtb@gmail.com>
2026-01-25 10:09:07 +0100

    new src

--- aprsmap_common/gm-hamnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# assumes script is called via aprsmap in the aprsmap directory
-# this script calls gm.sh with hamnet-specific map tile urls
-#
-# this script is selected by using the aprsmap.cfg entry:
-# Get Map Program|1|gm-hamnet.sh
-
-./gm.sh $1 $2 maplist-hamnet
